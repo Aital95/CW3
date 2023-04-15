6011defb4c50bfca5db5c0c8f08058009f90df12
user <aytem95@gmail.com>
2023-04-16 01:07:03 +0600

    add commit CW3

---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+from utils import sort_data
+
+
+def test_sort_data(test_data):
+    sorted_data = sort_data(test_data)
+    assert [x['date'] for x in sorted_data] == ['2019-07-03T18:35:29.512364', '2018-06-30T02:08:58.425572', '2017-08-26T10:50:58.294041']
---
+++ utils.py
@@ -0,0 +1,52 @@
+import json
+from datetime import datetime
+
+
+def get_data():
+    with open('operations.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+def filter_data(data):
+    data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
+    return data
+
+
+def sort_data(data):
+    data = sorted(data, key=lambda x: x['date'], reverse=True)
+    return data[:5]
+
+
+def format_data(data):
+    formatted_data = []
+    for row in data:
+        date = datetime.strptime(row['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime("%d.%m.%Y")
+        description = row['description']
+        if "from" in row:
+            from_arrow = "->"
+            sender = row['from'].split()
+            sender_bill = sender.pop(-1)
+            sender_info = " ".join(sender)
+            sender_bill = f"{sender_bill[:4]} {sender_bill[4:6]}** **** {sender_bill[-4:]}"
+            recipient = row['to'].split()
+            recipient_bill = recipient.pop(-1)
+            recipient_bill = f"**{recipient_bill[-4:]}"
+            transfer_summ = row['operationAmount']['amount']
+            currency = row['operationAmount']['currency']['name']
+        else:
+            sender_info = "Новый счет"
+            sender_bill = ""
+            from_arrow = ""
+            recipient_bill = ""
+            transfer_summ = ""
+            currency = ""
+
+
+
+        formatted_data.append(f"""
+{date} {description}
+{sender_info} {sender_bill} {from_arrow} {recipient_bill}
+{transfer_summ} {currency}\
+        """)
+    return formatted_data
